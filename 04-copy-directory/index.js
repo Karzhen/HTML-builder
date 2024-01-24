3c01765698a4bdf5d30348a351efb23cdc087e2b
Egor Cherkashin <egor9298@mail.ru>
2024-01-24 14:24:34 +0300

    refactor: correction 04-copy-directory Wed, Jan 24, 2024 02:24:34 PM

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,31 +1,63 @@
 const fs = require('fs');
-const path = require('node:path');
+const path = require('path');
 const filesFolderPath = path.join(__dirname, 'files');
 const filesCopyPath = path.join(__dirname, 'files-copy');
 
-fs.mkdir(filesCopyPath, { recursive: true }, (error) => {
-  if (error) {
-    console.error('The error occurred', error);
-    process.exit(1);
-  }
-  console.log('Directory created successfully!');
-});
+async function copyFiles(sourcePath, destinationPath) {
+  try {
+    // Обработка обнуления папки
+    try {
+      await fs.promises.access(destinationPath);
+      await fs.promises.rm(destinationPath, { force: true, recursive: true });
+      console.log(`Removed existing folder: ${destinationPath}`);
+    } catch (error) {
+      /* empty */
+    }
+    await fs.promises.mkdir(destinationPath, { recursive: true });
+    console.log(`Directory ${destinationPath} created successfully!`);
 
-fs.readdir(filesFolderPath, (error, files) => {
-  if (error) {
-    console.error('The error occurred', error);
-    process.exit(1);
-  }
+    const files = await fs.promises.readdir(sourcePath);
+    const filesCopy = await fs.promises.readdir(destinationPath);
 
-  files.forEach((file) => {
-    const sourceFilePath = path.join(filesFolderPath, file);
-    const destinationFilePath = path.join(filesCopyPath, file);
-    fs.copyFile(sourceFilePath, destinationFilePath, (error) => {
-      if (error) {
-        console.error('Error when copying a file:', error);
-      } else {
-        console.log(`The file ${file} has been successfully copied.`);
+    for (const file of filesCopy) {
+      if (!files.includes(file)) {
+        const filePathToRemove = path.join(destinationPath, file);
+        await fs.promises.unlink(filePathToRemove);
+        console.log(`File ${file} has been removed from files-copy.`);
       }
-    });
-  });
-});
+    }
+
+    for (const file of files) {
+      const sourceFilePath = path.join(sourcePath, file);
+      const destinationFilePath = path.join(destinationPath, file);
+
+      // const sourceStat = await fs.promises.stat(sourceFilePath);
+      // if (sourceStat.isDirectory()) {
+      //   await copyFiles(sourceFilePath, destinationFilePath);
+      // } else {
+      //   const destinationStat = await fs.promises
+      //     .stat(destinationFilePath)
+      //     .catch(() => null);
+      //
+      //   if (!destinationStat || sourceStat.mtimeMs > destinationStat.mtimeMs) {
+      //     await fs.promises.copyFile(sourceFilePath, destinationFilePath);
+      //     console.log(`File ${file} has been updated in files-copy.`);
+      //   }
+      // }
+      const sourceStat = await fs.promises.stat(sourceFilePath);
+      const destinationStat = await fs.promises
+        .stat(destinationFilePath)
+        .catch(() => null);
+
+      if (!destinationStat || sourceStat.mtimeMs > destinationStat.mtimeMs) {
+        await fs.promises.copyFile(sourceFilePath, destinationFilePath);
+        console.log(`File ${file} has been updated in files-copy.`);
+      }
+    }
+  } catch (error) {
+    console.error('Error occurred:', error);
+    process.exit(1);
+  }
+}
+
+copyFiles(filesFolderPath, filesCopyPath);
--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -4,15 +4,15 @@ const stylesFolderPath = path.join(__dirname, 'styles');
 const projectPath = path.join(__dirname, 'project-dist');
 const bundlePath = path.join(projectPath, 'bundle.css');
 
-const copyStyles = async () => {
+async function copyStyles(stylesPath, projectDistPath) {
   try {
-    const files = await fs.promises.readdir(stylesFolderPath);
+    const files = await fs.promises.readdir(stylesPath);
     const cssFiles = files.filter((file) => path.extname(file) === '.css');
     const readPromises = cssFiles.map((file) =>
-      fs.promises.readFile(path.join(stylesFolderPath, file), 'utf8'),
+      fs.promises.readFile(path.join(stylesPath, file), 'utf8'),
     );
     const contents = await Promise.all(readPromises);
-    const writeStream = fs.createWriteStream(bundlePath);
+    const writeStream = fs.createWriteStream(projectDistPath);
 
     for (const content of contents) {
       writeStream.write(content);
@@ -22,6 +22,6 @@ const copyStyles = async () => {
     console.error('The error occurred', error);
     process.exit(1);
   }
-};
+}
 
-copyStyles();
+copyStyles(stylesFolderPath, bundlePath);
